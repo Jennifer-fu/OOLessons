043a9d2f7b1bc03cc29717cc9eb13fcd27038953
unknown <jffu@jffu-PC.(none)>
2012-10-18 13:39:33 +0800

    finished equality function

---
+++ OOLessons/src/Chance.cs
@@ -0,0 +1,26 @@
+namespace OOLessons.test
+{
+    public class Chance
+    {
+        private readonly double possibility;
+
+        public Chance(double possibility)
+        {
+            this.possibility = possibility;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof (Chance)) return false;
+            var other = (Chance) obj;
+            return other.possibility.Equals(possibility);
+        }
+
+        public override int GetHashCode()
+        {
+            return possibility.GetHashCode();
+        }
+    }
+}
--- OOLessons/test/ChanceTest.cs
+++ OOLessons/test/ChanceTest.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using OOLessons.test;
 
 namespace OOLessons
 {
-    public class Class1
+    public class ChanceTest
     {
+        [Test]
+        public void chance_of_0dot1_should_equal_chance_of_0dot1()
+        {
+            var chanceOf0Dot1 = new Chance(0.1);
+            var anotherChanceOf0Dot1 = new Chance(0.1);
+
+            Assert.AreEqual(chanceOf0Dot1,anotherChanceOf0Dot1);
+        }
+
+        [Test]
+        public void chance_of_0dot1_should_equal_chance_of_0dot2()
+        {
+            var chanceOf0Dot1 = new Chance(0.1);
+            var chanceOf0Dot2 = new Chance(0.2);
+
+            Assert.AreNotEqual(chanceOf0Dot1, chanceOf0Dot2);
+        }
+
+        [Test]
+        public void chance_of_0dot1_should_not_equal_any_object()
+        {
+            var chanceOf0Dot1 = new Chance(0.1);
+
+            Assert.AreNotEqual(chanceOf0Dot1, new Object());
+        }
     }
 }
